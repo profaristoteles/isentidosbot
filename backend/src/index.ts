fc52bf6210153c25624568bf9ce176e643d45e83
Aristoteles Meneses Lima <aristoteles@isentidos.com.br>
2026-08-18 09:50:19 -0300

    fix(express): increase json and urlencoded body parser payload limit to 50mb

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -27,8 +27,8 @@ const PORT = process.env.PORT || 5000;
 // Middlewares Globais
 app.use(helmet({ crossOriginResourcePolicy: false }));
 app.use(cors());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 
 // Servir arquivos de upload estáticos (PDFs, imagens agendadas)
 app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
